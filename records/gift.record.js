08f75bb9fcf28849847ed216a3e57056605f6cf7
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-30 13:35:23 +0200

    insert method built 30.08.23 13:35

--- records/gift.record.js
+++ records/gift.record.js
@@ -1,5 +1,6 @@
 const { pool } = require("../utils/db");
 const { ValidationError } = require("../utils/error/error");
+const { v4: uuid } = require('uuid')
 
 class GiftRecord {
     constructor(obj) {
@@ -10,9 +11,21 @@ class GiftRecord {
         if (!obj.count || obj.count < 1 || obj.count > 99999) {
             throw new ValidationError('Gifts count has to be between 1 - 999999 pieces')
         }
+        this.id = obj.id;
         this.name = obj.name;
         this.count = obj.count;
-        this.id = obj.id;
+    }
+
+    async insert() {
+        if (!this.id) {
+            this.id = uuid()
+        }
+        await pool.execute("INSERT INTO `gifts` VALUES(:id, :name, :count)", {
+            id: this.id,
+            name: this.name,
+            count: this.count,
+        });
+        return this.id;
     }
 
     static async listAll() {
