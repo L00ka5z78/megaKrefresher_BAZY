55ca591aad6421e6dc1e434795f22c17276f06bd
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-08-30 13:26:54 +0200

    gift.record basic validation added 30.08.23 13:26

--- records/gift.record.js
+++ records/gift.record.js
@@ -1,27 +1,23 @@
+const { pool } = require("../utils/db");
+const { ValidationError } = require("../utils/error/error");
+
 class GiftRecord {
-    static listAll() {
-        return [
-            {
-                id: 'abc1',
-                name: 'Dolly house',
-                count: 5,
-            },
-            {
-                id: 'abc2',
-                name: 'Car',
-                count: 3
-            },
-            {
-                id: 'abc3',
-                name: 'Knife',
-                count: 5
-            },
-            {
-                id: 'abc4',
-                name: 'Battle axe',
-                count: 6
-            },
-        ];
+    constructor(obj) {
+        if (!obj.name || obj.name.length < 3 || obj.name.length > 55) {
+            throw new ValidationError('Name has to be between 3 - 55 characters')
+        }
+
+        if (!obj.count || obj.count < 1 || obj.count > 99999) {
+            throw new ValidationError('Gifts count has to be between 1 - 999999 pieces')
+        }
+        this.name = obj.name;
+        this.count = obj.count;
+        this.id = obj.id;
+    }
+
+    static async listAll() {
+        const [results] = await pool.execute("SELECT * FROM `gifts`");
+        return results
     }
 }
 module.exports = {
